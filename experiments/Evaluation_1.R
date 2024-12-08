9591b542f994b3d6e95c96ef2ad380814142d43e
AdriGl117 <148889279+AdriGl117@users.noreply.github.com>
2024-12-08 14:05:29 +0100

    Add documentation to Evaluation_1.R

--- experiments/Evaluation_1.R
+++ experiments/Evaluation_1.R
@@ -1,13 +1,14 @@
+###### Evalutaion Script for Benchmark Experiment #######
 source("sources.R")
 
 ##### Create Result Object #####
-
 # Read result object
 bmr1 <- readRDS("experiments/bmr_Exp_1.rds")
 
 # Select measure
 measures <- c(msr("classif.ece"), msr("classif.bbrier"), msr("classif.logloss"))
 
+# Aggregate results Object
 res = bmr1$aggregate(measures)
 res = res[, .(task_id, learner_id, classif.ece, classif.bbrier, classif.logloss)]
 
@@ -21,12 +22,13 @@ res[, Calibrator := ifelse(grepl("uncalibrated", learner_id), "uncalibrated",
 
 # Add column Resampling
 res[,Resampling := ifelse(grepl("5", learner_id), "cv_5",
-                          ifelse(grepl("3", learner_id), "cv_3", 
-                                 ifelse(grepl("70", learner_id), "holdout_70",
-                                        ifelse(grepl("80", learner_id), "holdout_80",
-                                               ifelse(grepl("90", learner_id), "holdout_90", 
-                                                      ifelse(grepl("union", learner_id), "union", "uncalibrated"
-                                               ))))))]
+                    ifelse(grepl("3", learner_id), "cv_3", 
+                    ifelse(grepl("70", learner_id), "holdout_70",
+                    ifelse(grepl("80", learner_id), "holdout_80",
+                    ifelse(grepl("90", learner_id), "holdout_90", 
+                    ifelse(grepl("union", learner_id), "union", "uncalibrated"
+                    ))))))]
+
 # Add coloumn learner
 res[, Learner := gsub("(.*) .* .*", "\\1", learner_id)]
 res[, Learner := gsub("(.*) .*", "\\1", Learner)]
@@ -39,25 +41,33 @@ res <- res %>%
     task_id %in% c("electricity", "nomao", "numerai28.6") ~ "large"
   ))
 
-res = res[, .(Learner, Calibrator, Resampling, task_id, task_size, classif.ece, classif.bbrier, classif.logloss)]
+# Select relevant columns
+res = res[, .(Learner, Calibrator, Resampling, task_id, task_size, classif.ece, 
+              classif.bbrier, classif.logloss)]
 
 ##### Results per Resampling Strategie #####
-
-# Group by Resampling
+# Pivot on Resampling
 measures <- c("classif.ece", "classif.bbrier", "classif.logloss")
-resampling_columns <- c("holdout_70", "holdout_80", "holdout_90", "cv_3", "cv_5", "union")
+resampling_columns <- c("holdout_70", "holdout_80", "holdout_90", "cv_3", 
+                        "cv_5", "union")
 for (measure in measures) {
+  # Creates a pivot table for the resampling strategies once for all measures, 
+  # plots and save the CD plots, create average ranks and scores and save the
+  # results in a combined data.table
   res_resamplings_pivot  <- data.table::dcast(
     data = res,
     formula = Learner + Calibrator + task_id ~ Resampling,
     value.var = measure
   )
   res_resamplings_pivot <- res_resamplings_pivot %>%
-    select(-starts_with("uncalibrated"), -starts_with("Learner"), -starts_with("Calibrator"), -starts_with("task_id"))
+    select(-starts_with("uncalibrated"), -starts_with("Learner"),
+           -starts_with("Calibrator"), -starts_with("task_id"))
   p <- cd_plot(res_resamplings_pivot) 
   ggsave(paste0("figures/Exp_1_Resampling_", measure, ".jpeg"), dpi = 300)
-  res_resamplings_pivot <- res_resamplings_pivot[complete.cases(res_resamplings_pivot)]
-  rank_matrix <- t(apply(as.data.frame(res_resamplings_pivot), 1, rank, ties.method = "average"))
+  res_resamplings_pivot <- res_resamplings_pivot[
+    complete.cases(res_resamplings_pivot)]
+  rank_matrix <- t(apply(as.data.frame(res_resamplings_pivot),
+                         1, rank, ties.method = "average"))
   rank_dt <- as.data.table(rank_matrix)
   setnames(rank_dt, paste0(names(rank_dt), "_rank"))
   res_resamplings_pivot <- cbind(res_resamplings_pivot, rank_dt)
@@ -69,19 +79,24 @@ for (measure in measures) {
     Mean = round(as.numeric(Means),4),
     Rank = round(as.numeric(Ranks),2)
   )
-  colnames(df) = c("Resampling", paste0(measure,"_mean"), paste0(measure,"_rank"))
+  colnames(df) = c("Resampling", paste0(measure,"_mean"), 
+                   paste0(measure,"_rank"))
   if (!exists("res_resampling")) {
     res_resampling  <- df
   }else{
     res_resampling  <- merge(res_resampling, df, by = "Resampling")
   }
 }
+# Print results
 print(res_resampling)
 
-# Group by task_size and resampling
+# Pivot in resampling, grouped by Task size
 size_values <- c("small", "medium", "large")
 for (measure in measures) {
   for (size_value in size_values) {
+    # Creates a pivot table for the resampling strategies once for all measures 
+    # and task size, plots and save the CD plots, create average ranks and 
+    # scores and save the results in a combined data.table
     res_filtered <- res %>% filter(task_size == size_value)
     res_resamplings_pivot  <- data.table::dcast(
       data = res_filtered,
@@ -125,15 +140,20 @@ for (measure in measures) {
     }
   }
 }
+# Print the results
 print(res_resampling_grouped)
 
 ##### Results per Calibrator #####
 
-# Group by Calibrator
+# Pivot on Calibration Approach
 measures <- c("classif.ece", "classif.bbrier", "classif.logloss")
 calibrator_columns <- c("platt", "beta", "isotonic", "uncalibrated")
 for (measure in measures) {
-  res_filtered <- res %>% filter(Resampling == "uncalibrated" | Resampling == "cv_5")
+  # Creates a pivot table for the approaches once for all measures,
+  # plots and save the CD plots, create average ranks and scores and save the
+  # results in a combined data.table
+  res_filtered <- res %>% filter(Resampling == "uncalibrated" | 
+                                   Resampling == "cv_5")
   res_calibrators_pivot  <- data.table::dcast(
     data = res_filtered,
     formula = Learner + task_id ~ Calibrator,
@@ -143,7 +163,8 @@ for (measure in measures) {
     select(-starts_with("Learner"), -starts_with("task_id"))
   p <- cd_plot(res_calibrators_pivot) 
   ggsave(paste0("figures/Exp_1_Calibrator_", measure, ".jpeg"), dpi = 300)
-  rank_matrix <- t(apply(as.data.frame(res_calibrators_pivot), 1, rank, ties.method = "average"))
+  rank_matrix <- t(apply(as.data.frame(res_calibrators_pivot), 
+                         1, rank, ties.method = "average"))
   rank_dt <- as.data.table(rank_matrix)
   setnames(rank_dt, paste0(names(rank_dt), "_rank"))
   res_calibrators_pivot <- cbind(res_calibrators_pivot, rank_dt)
@@ -155,19 +176,24 @@ for (measure in measures) {
     Mean = round(as.numeric(Means),4),
     Rank = round(as.numeric(Ranks),2)
   )
-  colnames(df) = c("Calibrator", paste0(measure,"_mean"), paste0(measure,"_rank"))
+  colnames(df) = c("Calibrator", paste0(measure,"_mean"), 
+                   paste0(measure,"_rank"))
   if (!exists("res_calibrator")) {
     res_calibrator  <- df
   }else{
     res_calibrator  <- merge(res_calibrator, df, by = "Calibrator")
   }
 }
+# Print the results
 print(res_calibrator)
 
-# Group by task_size calibrator
+# Pivot on calibration, grouped by Task size
 size_values <- c("small", "medium", "large")
 for (measure in measures) {
   for (size_value in size_values) {
+    # Creates a pivot table for the calibration approaches once for all measures 
+    # and task size, plots and save the CD plots, create average ranks and 
+    # scores and save the results in a combined data.table
     res_filtered <- res %>% filter(task_size == size_value & 
                     (Resampling == "uncalibrated" | Resampling == "cv_5"))
     res_calibrator_pivot  <- data.table::dcast(
@@ -177,10 +203,13 @@ for (measure in measures) {
     )
     res_calibrator_pivot <- res_calibrator_pivot %>%
       select(-starts_with("Learner"), -starts_with("task_id"))
-    res_calibrator_pivot <- res_calibrator_pivot[complete.cases(res_calibrator_pivot)]
+    res_calibrator_pivot <- res_calibrator_pivot[
+      complete.cases(res_calibrator_pivot)]
     p <- cd_plot(res_calibrator_pivot)
-    ggsave(paste0("figures/Exp_1_Calibrator_", size_value, "_", measure, ".jpeg"), dpi = 300)
-    rank_matrix <- t(apply(as.data.frame(res_calibrator_pivot), 1, rank, ties.method = "average"))
+    ggsave(paste0("figures/Exp_1_Calibrator_", 
+                  size_value, "_", measure, ".jpeg"), dpi = 300)
+    rank_matrix <- t(apply(as.data.frame(res_calibrator_pivot), 
+                           1, rank, ties.method = "average"))
     rank_dt <- as.data.table(rank_matrix)
     setnames(rank_dt, paste0(names(rank_dt), "_rank"))
     res_calibrator_pivot <- cbind(res_calibrator_pivot, rank_dt)
@@ -193,7 +222,8 @@ for (measure in measures) {
       Mean = round(as.numeric(Means),4),
       Rank = round(as.numeric(Ranks),2)
     )
-    colnames(df) = c("Size", "Calibrator", paste0(measure,"_mean"), paste0(measure,"_rank"))
+    colnames(df) = c("Size", "Calibrator", paste0(measure,"_mean"), 
+                     paste0(measure,"_rank"))
     if(!exists("res_per_measure")) {
       res_per_measure <- df
     } else {
@@ -214,11 +244,16 @@ for (measure in measures) {
 print(res_calibrator_grouped)
 
 #### Results per Calibrator and Resampling Strategy #####
+# Pivot on Resampling, gruped by Calibrator
 measures <- c("classif.ece", "classif.bbrier", "classif.logloss")
-resampling_columns <- c("holdout_70", "holdout_80", "holdout_90", "cv_3", "cv_5", "union")
+resampling_columns <- c("holdout_70", "holdout_80", "holdout_90",
+                        "cv_3", "cv_5", "union")
 calibrator_columns <- c("platt", "beta", "isotonic")
 for(measure in measures){
   for(calibrator_value in calibrator_columns){
+    # Creates a pivot table for the resampling strategies once for all measures 
+    # and calibratprs, plots and save the CD plots, create average ranks and 
+    # scores and save the results in a combined data.table
     res_filtered <- res %>% filter(Calibrator == calibrator_value)
     res_calibrator_pivot  <- data.table::dcast(
       data = res_filtered,
@@ -226,10 +261,13 @@ for(measure in measures){
       value.var = measure
     )
     res_calibrator_pivot <- res_calibrator_pivot %>%
-      select(-starts_with("Learner"), -starts_with("task_id"), -starts_with("Calibrator"))
+      select(-starts_with("Learner"), -starts_with("task_id"), 
+             -starts_with("Calibrator"))
     p <- cd_plot(res_calibrator_pivot)
-    ggsave(paste0("figures/Exp_1_Resamplings_", calibrator_value, "_", measure, ".jpeg"), dpi = 300)
-    rank_matrix <- t(apply(as.data.frame(res_calibrator_pivot), 1, rank, ties.method = "average"))
+    ggsave(paste0("figures/Exp_1_Resamplings_", calibrator_value, 
+                  "_", measure, ".jpeg"), dpi = 300)
+    rank_matrix <- t(apply(as.data.frame(res_calibrator_pivot),
+                           1, rank, ties.method = "average"))
     rank_dt <- as.data.table(rank_matrix)
     setnames(rank_dt, paste0(names(rank_dt), "_rank"))
     res_calibrator_pivot <- cbind(res_calibrator_pivot, rank_dt)
@@ -242,7 +280,8 @@ for(measure in measures){
       Mean = round(as.numeric(Means),4),
       Rank = round(as.numeric(Ranks),2)
     )
-    colnames(df) = c("Calibrator", "Resampling", paste0(measure,"_mean"), paste0(measure,"_rank"))
+    colnames(df) = c("Calibrator", "Resampling", paste0(measure,"_mean"), 
+                     paste0(measure,"_rank"))
     if(!exists("res_per_measure")) {
       res_per_measure <- df
     } else {
@@ -252,25 +291,29 @@ for(measure in measures){
       if (!exists("res_calibrator_resampling_grouped")) {
         res_calibrator_resampling_grouped  <- res_per_measure
       }else{
-        res_calibrator_resampling_grouped  <- merge(res_calibrator_resampling_grouped, res_per_measure, 
+        res_calibrator_resampling_grouped  <- merge(
+          res_calibrator_resampling_grouped, res_per_measure, 
                                          by = c("Calibrator", "Resampling"))
       }
       remove(res_per_measure)
     }
   }
 }
+# Print results
 print(res_calibrator_resampling_grouped)
-##### Results per Learner and Calibrator#####
 
-# Group by Learner and Calibrator
+##### Results per Learner and Calibrator#####
+# Pivot on Calibration, grouped by Learner
 res_learner = res %>% 
   filter(Resampling == "uncalibrated" | Resampling == "cv_5") %>%
   select(-contains("Resampling"), -contains("task_size"))
-#res_learner$Resampling = ifelse(res_learner$Resampling == "uncalibrated", "uncalibrated", "calibrated")
 columns <- c("platt", "beta", "isotonic", "uncalibrated")
 learner_values <- unique(res_learner$Learner)
 for(measure in measures){
   for(learner_value in learner_values){
+    # Creates a pivot table for the calibration approaches once for all measures 
+    # and learners, create average ranks and scores and save the results in a 
+    # combined data.table
     res_learner_filtered <- res_learner %>% filter(Learner == learner_value)
     res_learner_pivot  <- data.table::dcast(
       data = res_learner_filtered,
@@ -279,7 +322,8 @@ for(measure in measures){
     )
     res_learner_pivot <- res_learner_pivot %>%
       select(-starts_with("Learner"), -starts_with("task_id"))
-    rank_matrix <- t(apply(as.data.frame(res_learner_pivot), 1, rank, ties.method = "average"))
+    rank_matrix <- t(apply(as.data.frame(res_learner_pivot),
+                           1, rank, ties.method = "average"))
     rank_dt <- as.data.table(rank_matrix)
     setnames(rank_dt, paste0(names(rank_dt), "_rank"))
     res_learner_pivot <- cbind(res_learner_pivot, rank_dt)
@@ -292,7 +336,8 @@ for(measure in measures){
       Mean = round(as.numeric(Means),4),
       Rank = round(as.numeric(Ranks),2)
     )
-    colnames(df) = c("Learner", "Calibrator", paste0(measure,"_mean"), paste0(measure,"_rank"))
+    colnames(df) = c("Learner", "Calibrator", paste0(measure,"_mean"), 
+                     paste0(measure,"_rank"))
     if(!exists("res_per_measure")) {
       res_per_measure <- df
     } else {
@@ -309,4 +354,5 @@ for(measure in measures){
     }
   }
 }
+# Print results
 print(res_learner_grouped)
